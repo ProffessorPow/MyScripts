9e9692334e0331d9d792996c36a8d0a5735958be
Jarrett Randall <jarrett.randall3@gmail.com>
2022-09-02 12:04:47 -0600

    Added the scripts from the Matchgame

---
+++ Matchgame Scripts/IntData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntData : ScriptableObject
+{
+    public int value;
+    
+    public void UpdateValue(int num)
+    {
+        value += num;
+    }
+}
