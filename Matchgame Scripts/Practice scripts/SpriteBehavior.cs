059f0bb4aa4a9ed1dbe8387c68ad1eda1792828f
Jarrett Randall <jarrett.randall3@gmail.com>
2022-09-14 23:36:12 -0600

    Scripts from the lab

---
+++ Matchgame Scripts/Practice scripts/SpriteBehavior.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteBehavior : MonoBehaviour
+{
+    private SpriteRenderer rendererObj;
+
+    private void Awake()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeRendererColor(ColorID obj)
+    {
+        rendererObj.color = obj.value;
+    }
+}
